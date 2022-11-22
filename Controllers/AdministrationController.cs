69df6956f8d9a9be0338ad83c1a4a7a974d252ad
Teenamx <teenamx777@gmail.com>
2022-11-23 04:13:54 +0530

    Delete User and role from identity database

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -21,7 +21,54 @@ namespace EmployeeManagement1.Controllers
             this.roleManager = roleManager;
             this.userManager = userManager;
         }
-     
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id={id} cannot be found";
+                return View();
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View("ListUsers");
+
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id={id} cannot be found";
+                return View();
+            }
+            else
+            {
+                var result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View("ListRoles");
+
+            }
+        }
 
         [HttpGet]
         public IActionResult CreateRole()
